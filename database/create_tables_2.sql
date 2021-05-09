733cb7263214990a9edcdf500c8bddfbb720f632
Kristoffer <kristoffer.pbs@gmail.com>
2021-05-09 17:04:24 +0200

    fix: it is fixed

--- database/create_tables_2.sql
+++ database/create_tables_2.sql
@@ -5,7 +5,7 @@ USE lightguide_dev;
 
 CREATE TABLE events (
     id int PRIMARY KEY AUTO_INCREMENT,
-    ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, COMMENT 'Format is YYYY-MM-DD HH:MM:SS'
+    ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Format is YYYY-MM-DD HH:MM:SS',
     event_type_id int NOT NULL,
     patient_id varchar(255) NOT NULL,
     gateway_id varchar(255) NOT NULL
@@ -18,7 +18,7 @@ CREATE TABLE event_types (
 
 CREATE TABLE patients (
     id int PRIMARY KEY AUTO_INCREMENT,
-    patient_id varchar(255) UNIQUE NOT NULL
+    patient_id varchar(255) UNIQUE NOT NULL,
     full_name varchar(255) NOT NULL
 );
 
@@ -70,6 +70,8 @@ INSERT INTO event_types(event_type) VALUES
     ('notification');
 
 INSERT INTO patients(patient_id, full_name) VALUES('041cb23-31f4-4b27-a20b-d160564e2e68', 'test_patient');
+
+INSERT INTO caregivers(caregiver_id, username, login_credential_hash) VALUES('fef16dcd-87d2-4f2d-a92e-7af18dd605a7', 'caregiver', '3f27b5bf43f45bc9142d9057b78869637f62d8b7ea705403411302c2a6970edb');
 
 INSERT INTO gateways(gateway_id) VALUES('1fb3b683-7fd5-4581-b201-30ac171e5414');
 
