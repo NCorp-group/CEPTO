78ad0223adf3cc553f12f3f4d8b7c760a2ad77b3
Lorenzo Panieri <panierilorenzo@gmail.com>
2021-05-19 18:57:14 +0200

    cleanup

--- web_server/install.sh
+++
@@ -1,9 +0,0 @@
-# Set-up python virtual environment
-sudo apt update
-sudo apt install python3-pip -y
-sudo apt install python3-venv -y
-python3 -m venv .
-source ./bin/activate
-
-# install PIP dependencies
-pip install -r requirements.txt
--- web_server/launch.sh
+++ web_server/launch.sh
@@ -1,2 +1,12 @@
-python backend_api/mqtt2db.py &
-python web_api/front_api.py &
+# Set-up python virtual environment
+sudo apt update
+sudo apt install python3-pip -y
+sudo apt install python3-venv -y
+python3 -m venv .
+source ./bin/activate
+
+# install PIP dependencies
+pip install -r requirements.txt
+
+# launch
+python3 main.py
