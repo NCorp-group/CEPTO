1c3cd8e5d1a17700152cca42ed8dbe19949ce005
Kristoffer-PBS <kristoffer.pbs@gmail.com>
2021-05-23 19:13:31 +0200

    fix: changed launch.sh to installation.sh

--- web_server/install.sh
+++ web_server/install.sh
@@ -7,8 +7,6 @@ sudo apt update -y && sudo apt install -y \
     python3-venv \
     mosquitto \
     mariadb-server
-# sudo apt install python3-pip -y
-# sudo apt install python3-venv -y
 
 sudo systemctl enable mosquitto.service
 sudo systemctl start mosquitto.service
@@ -16,18 +14,5 @@ sudo systemctl start mosquitto.service
 sudo systemctl enable mariadb.service
 sudo systemctl start mariadb.service
 
-echo -e "TYPE:\n\tGRANT ALL ON *.* TO 'testuser'@'localhost' IDENTIFIED BY 'test' WITH GRANT OPTION;"
-
-sudo mariadb
-
-if [ ! -f ./bin/activate ]; then 
-    /usr/bin/python3 -m venv .
-fi
-
-source ./bin/activate
-
 # install PIP dependencies
 pip install -r requirements.txt
-
-# launch
-# python3 main.py
