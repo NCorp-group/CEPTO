32d74f505a126191f8a5f49bd622b3665603ad6f
Kristoffer-PBS <57013304+Kristoffer-PBS@users.noreply.github.com>
2021-05-25 15:26:40 +0200

    fix: changed python to python3

--- web_server/main.py
+++ web_server/main.py
@@ -2,8 +2,8 @@ import subprocess as sp
 import time
 
 if __name__ == "__main__":
-    p2 = sp.Popen(['python', 'back_api.py'])
-    p1 = sp.Popen(['python', 'front_api.py'])
+    p2 = sp.Popen(['python3', 'back_api.py'])
+    p1 = sp.Popen(['python3', 'front_api.py'])
 
     time.sleep(5)
     print("Server launch complete")
